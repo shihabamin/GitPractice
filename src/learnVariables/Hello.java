a4bffd7c1d2b4d50bb19ef6a7d825ad51b2881e3
shihabamin <amin.khandaker@gmail.com>
2020-04-15 20:00:17 -0500

    commit 2

---
+++ src/learnVariables/Hello.java
@@ -0,0 +1,13 @@
+package learnVariables;
+
+public class Hello {
+public static void main(String[] args) {
+		
+		System.out.println("Hello World!!!");
+		System.out.println("Hello World!!!");
+		System.out.println("Hello World!!!");
+		System.out.println("Hello World!!!");
+		
+	}
+
+}
